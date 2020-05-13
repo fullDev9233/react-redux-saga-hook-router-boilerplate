760931be4d50c58370ce8723201a227b172215f8
jesus9233 <jimyy9211@gmail.com>
2020-05-13 16:55:31 -0500

    v1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>App</h1>
+//       <button onClick={}></button>
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  render() {
+    return(
+      <div className='App'>
+        <Router>
+          <button>
+            <Link to='/abs'>
+              Search
+            </Link>
+          </button>
+        </Router>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
